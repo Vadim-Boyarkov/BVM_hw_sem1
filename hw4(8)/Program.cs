7ef5a0cd234ba9c18cf1b8be1d2c01411cabcf69
Vadim-Boyarkov <fluentzhiv@gmail.com>
2022-08-31 22:44:55 +0500

    четвертая задача (терминал ложится) (№8)

---
+++ hw4(8)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите положительное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number < 1)
+{
+    Console.WriteLine("Число не положительное");
+}
+else
+{
+int NumberA = 1;
+while (NumberA <= Number) ;
+int ost = NumberA % 2;
+if (ost == 0)
+{
+    Console.WriteLine(NumberA);
+    NumberA = NumberA + 1;
+}
+}
